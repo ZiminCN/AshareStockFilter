69baf28e8dad3cafc69c92970806d906695345f3
Violet-Zimin <jianming.zeng@directdrivetech.com>
2026-04-09 19:01:28 +0800

    trival: add apache2.0 License
    Signed-off-by: Violet-Zimin <jianming.zeng@directdrivetech.com>

--- system/input.py
+++ system/input.py
@@ -1,5 +1,4 @@
-# Copyright (c) Direct Drive Technology Co., Ltd. All rights reserved.
-# Author: Zi Min <jianming.zeng@directdrivetech.com>
+# Copyright (c) 2026 Ziminyo. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
 
 class Input:
     def __init__(self):
--- utils/log.py
+++ utils/log.py
@@ -1,5 +1,4 @@
-# Copyright (c) Direct Drive Technology Co., Ltd. All rights reserved.
-# Author: Zi Min <jianming.zeng@directdrivetech.com>
+# Copyright (c) 2026 Ziminyo. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
